1fb2dc0ff3aac738dd2c4b3eadb61c2bda04c149
TANJAKA Jerry Jacques <tanjakajerryjacques@gmail.com>
2026-01-22 15:05:42 +0300

    update table adding stockmouvement and mouvement_type

--- src/main/resources/new_schema.sql
+++ src/main/resources/new_schema.sql
@@ -28,3 +28,17 @@ ALTER TABLE Dish ADD COLUMN IF NOT EXISTS selling_price NUMERIC;
 
 -- delete id_dish column in ingredient since it's not necessary anymore
 ALTER TABLE ingredient DROP COLUMN IF EXISTS id_dish;
+
+-- 1. Nouveau type pour différencier Entrée et Sortie
+CREATE TYPE mouvement_type AS ENUM ('IN', 'OUT');
+
+-- 2. Table des mouvements
+CREATE TABLE StockMovement (
+                               id SERIAL PRIMARY KEY,
+                               id_ingredient INT NOT NULL,
+                               quantity NUMERIC(10, 2) NOT NULL,
+                               type mouvement_type NOT NULL,
+                               unit unit_type NOT NULL, -- Utilise le type unit_type que tu as déjà (KG, L, PCS)
+                               creation_datetime TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+                               CONSTRAINT fk_ingredient_stock FOREIGN KEY (id_ingredient) REFERENCES Ingredient(id)
+);
