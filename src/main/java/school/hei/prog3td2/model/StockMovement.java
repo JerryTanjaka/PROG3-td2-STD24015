4f35113f0025eba608de3a8574ec33f063fbbb95
TANJAKA Jerry Jacques <tanjakajerryjacques@gmail.com>
2026-01-22 15:25:17 +0300

    feat: add stockMovement class

--- src/main/java/school/hei/prog3td2/model/StockMovement.java
+++ src/main/java/school/hei/prog3td2/model/StockMovement.java
@@ -1,4 +1,49 @@
 package school.hei.prog3td2.model;
 
+import java.time.Instant;
+
 public class StockMovement {
+    private Integer id;
+    private StockValue value;
+    private MovementTypeEnum type;
+    private Instant creationDatetime;
+
+    public StockMovement(Instant creationDatetime, Integer id, MovementTypeEnum type, StockValue value) {
+        this.creationDatetime = creationDatetime;
+        this.id = id;
+        this.type = type;
+        this.value = value;
+    }
+
+    public Instant getCreationDatetime() {
+        return creationDatetime;
+    }
+
+    public void setCreationDatetime(Instant creationDatetime) {
+        this.creationDatetime = creationDatetime;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public MovementTypeEnum getType() {
+        return type;
+    }
+
+    public void setType(MovementTypeEnum type) {
+        this.type = type;
+    }
+
+    public StockValue getValue() {
+        return value;
+    }
+
+    public void setValue(StockValue value) {
+        this.value = value;
+    }
 }
